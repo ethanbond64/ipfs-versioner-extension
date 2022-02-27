c67a18827b95af15605dd01868002e1e4e9e10ce
ethanbond64 <ethanbond39@gmail.com>
2022-02-27 16:03:51 -0700

    extensions todos

--- extension/background.js
+++ extension/background.js
@@ -1,1 +1,11 @@
 console.log("Background");
+// TODO
+// IF NOT YET TRACKED
+    // Provide entrypoint to diff and upload api
+// IF TRACKED 
+    // Compare to last version, if different upload the changes through the api (TODO)
+    // Load versions into page if they exist
+    // Diff view can be handled here, but would be best to do most of that with css
+
+
+
