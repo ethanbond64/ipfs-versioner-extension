fd4d14e2f6f94384eca6ed480e859c3567592bb9
ethanbond64 <ethanbond39@gmail.com>
2022-03-13 13:20:11 -0600

    migrate text selection handler to new extension dir

--- extension/src/background.js
+++ extension/src/background.js
@@ -7,9 +7,8 @@
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+    const message = `Hi ${sender.tab ? 'Con' : 'Pop'
+      }, my name is Bac. I am from Background. It's great to hear from you.`;
 
     // Log message coming from the `request` parameter
     console.log(request.payload.message);
@@ -19,3 +18,31 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     });
   }
 });
+
+
+// TODO 
+// content selection upload to ipfs
+// get the cid
+// send to the popup and display
+
+// 1. Content selection in console
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "sample context action",
+    title: "Save Version",
+    type: 'normal',
+    contexts: ['selection']
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(
+  (info) => {
+    chrome.tabs.create({
+      url: "http://www.urbandictionary.com/define.php?term=" + encodeURIComponent(info.selectionText)
+    });
+  });
+
+
+// 2. Message to popup, displayed in popup
+// 3. Add ipfs api and upload as string
