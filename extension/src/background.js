d7bf97f9dd8acc2ee35b774ede53114cefc00ba7
ethanbond64 <ethanbond39@gmail.com>
2022-03-13 23:58:02 -0600

    cleanup

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,16 +1,45 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
 
-// TODO 
-// content selection upload to ipfs
-// get the cid
-// send to the popup and display
+/////////////// DATA FLOW 1 METHODS /////////////// 
 
-// 1. Content selection in console
+//// On tab change message from content
+// ? cache these in local storage ? if not there, then lookup
+// lookup on ipfs, get the count of diffs, most recent diff date
+// return info to popup
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === 'POPUPREQ') {
+
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+
+      // Get url data from cache/ipfs here
+      var tabURL = tabs[0].url;
+
+      console.log("Data path 1 step 2");
+
+      sendResponse({
+        type: "BACKRES",
+        url: tabURL,
+        n: 3,
+        dateLast: "03/11/2022"
+      });
+      console.log("Msg sent");
+    });
+
+    // Retquired to keep message port alive until finished
+    return true;
+  }
+});
+
+/////////////// DATA FLOW 2 METHODS ///////////////
+
+// TODO
+
+// Context Menu POC
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
@@ -28,54 +57,3 @@ chrome.contextMenus.onClicked.addListener(
       url: "http://www.urbandictionary.com/define.php?term=" + encodeURIComponent(info.selectionText)
     });
   });
-
-/////////////// DATA FLOW 1 METHODS /////////////// 
-// var currentUrl = "";
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'POPUPREQ') {
-
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs) {
-      // Get url data from cache/ipfs here
-      var tabURL = tabs[0].url;
-      console.log("Data path 1 step 2");
-      sendResponse({
-        type: "BACKRES",
-        url: tabURL,
-        n: 3,
-        dateLast: "03/11/2022"
-      });
-      console.log("Msg sent");
-    });
-    return true;
-  }
-});
-
-
-
-
-// chrome.tabs.onActivated.addListener((activeInfo) => {
-//   currentUrl = activeInfo.url;
-//   // console.log("background: tab changed");
-// });
-
-//// On tab change message from content
-// ? cache these in local storage ? if not there, then lookup
-// lookup on ipfs, get the count of diffs, most recent diff date
-//update the current tab info localStorage
-
-// chrome.tabs.onActivated.addListener((activeInfo) => {
-//   console.log("rerender the popup");
-//   console.log(activeInfo.tabId);
-//   chrome.storage.sync.set({ "currentTabPopupInfo": { "id": activeInfo.tabId } }, function () {
-//     console.log('Value is set to ' + activeInfo.tabId);
-//   });
-// });
-
-
-  /////////////// DATA FLOW 2 METHODS ///////////////
-
-  // TODO
--- extension/src/contentScript.js
+++ extension/src/contentScript.js
@@ -2,36 +2,14 @@
 
   /////////////// DATA FLOW 1 METHODS /////////////// 
 
-  //// On changeTab 
-  // ask backgroud for ipfs info, to send to popup
-// chrome.tabs.onActivated.addListener((activeInfo) => {
-//   console.log("content: tab changed, send to background");
-//   // console.log(activeInfo.tabId);
-//   chrome.runtime.sendMessage(
-//     {
-//       type: 'CTOB',
-//       payload: {
-//         tabId: activeInfo.tabId,
-//         url: activeInfo.url
-//       },
-//     },
-//     response => {
-//       console.log(response.message);
-//     }
-//   );
-// });
-
-// chrome.tabs.onActivated.addListener((activeInfo) => {
-//   console.log("content: tab changed");
-// });
-  // On message from popup.js
+  //// On message from popup.js
     // rerender the modal (no confirm button)
     // make visible
   
-  // On click off modal
+  //// On click off modal
     // hide modal
 
-  // On diff click
+  //// On diff click
     // bring to front (might be able to do with css idk) 
 
   /////////////// DATA FLOW 2 METHODS ///////////////
