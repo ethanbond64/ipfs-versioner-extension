ddb7c06ffc5a65fecda40188e2493bc554b0f114
ethanbond64 <ethanbond39@gmail.com>
2022-03-17 21:46:30 -0600

    Data access api design

--- extension/src/background.js
+++ extension/src/background.js
@@ -30,21 +30,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }, function (tabs) {
 
       // Get url data from cache/ipfs here
-      var tabURL = tabs[0].url;
+      // var tabURL = tabs[0].url;
 
       console.log("Data path 1 step 2");
       // let ff = sampleFetch();
-      let ff = "pausing fetches for now";
-      console.log(ff);
+      // let ff = "pausing fetches for now";
+      // console.log(ff);
 
-      sendResponse({
-        type: "BACKRES",
-        fetched: ff,
-        url: tabURL,
-        n: Math.floor((Math.random() * 5)),
-        dateLast: "03/11/2022",
-        testData: [{ content: "!AA Content", date: "A Date" }, { content: "BB! Content", date: "B DDDDate" }, { content: "CC! Content", date: "C Date" }]
-      });
+      sendResponse(getAllInfo(tabs[0].url));
       console.log("Msg sent");
     });
 
@@ -84,3 +77,45 @@ chrome.contextMenus.onClicked.addListener(
     });
     // });
   });
+
+
+//// Data Access Methods
+
+function getAllInfo(url) {
+
+  // Lookup url entry in the distributed db
+
+  // get all other info (including cids) from the distributed db
+
+  // loop over cids and get version text
+
+  // build an object to send
+  return {
+    type: "BACKRES",
+    url: url,
+    n: Math.floor((Math.random() * 5)),
+    dateLast: "03/11/2022",
+    testData: [{ content: "!AA Content", date: "A Date" }, { content: "BB! Content", date: "B DDDDate" }, { content: "CC! Content", date: "C Date" }]
+  }
+
+}
+
+function getLatestVersion(url) {
+
+  // Lookup url entry in the distributed db
+
+  // get latest cid, date
+
+  // lookup cid and get text
+
+  return { content: "!AA Content", date: "A Date" }
+
+}
+
+function uploadNewVersion(url, text) {
+
+  // upload the text to ipfs and hold on to cid
+
+  // create db entry keyed by this url with version number, date, and [cid]
+
+}
